6f3b5e6b577bee467ad866aae3ec65b5f92d8b6a
StuvX <wtnash@gmail.com>
2019-11-17 22:35:33 -0800

    plugin working

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Display name of your plugin, as it will appear in the menu
-    displayName: 'My super plugin',
+    displayName: 'windy-plugin-Owlet.AA-CAES',
 
     // Place, where opening link to your plugin will appear. So far only
     // Allowed: 'contextmenu', 'menu'
@@ -9,6 +9,9 @@ module.exports = {
     // List of external libraries, that should be loaded before
     // your plugin is even mounted to the page
     // dependencies: ['https://unpkg.com/d3@5.7.0/dist/d3.min.js']
+		dependencies: ['https://unpkg.com/jstat@1.9.2/dist/jstat.js']
+		// dependencies: ['./Orphaned_Wells.mjs', "https://api4.windy.com/assets/libBoot.js", "https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"]
+
 
     // List of classnames that will be attached to your plugin upon mounting
     // className: 'plugin-lhpane plugin-mobile-fullscreen',
